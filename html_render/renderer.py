68bdb7e948451f9bb03d31bb8fbd6a13673bbd0c
Sergio Leiva <sergio.leiva.ocl@gmail.com>
2018-11-24 20:42:43 -0300

    Cambiar doc para que sea un atributo de la clase

--- html_render/renderer.py
+++ html_render/renderer.py
@@ -8,30 +8,29 @@ class Renderer:
         json_file = open(file, encoding='utf8')
         contents = json_file.read()
         self.data = json.loads(contents)
+        self.doc = Doc()
 
     def render_program(self):
-        doc = Doc()
         for semester in iter(r.data.get('semestres')):
-            with doc.tag('tr'):
-                self.render_semester(semester, doc)
-        return doc.getvalue()
+            with self.doc.tag('tr'):
+                self.render_semester(semester)
+        return self.doc.getvalue()
 
-    def render_semester(self, semester, doc):
-        with doc.tag('td', klass='rotate'):
-            doc.text('Semestre ' + semester.get('numero'))
+    def render_semester(self, semester):
+        with self.doc.tag('td', klass='rotate'):
+            self.doc.text('Semestre ' + semester.get('numero'))
         for ramo in iter(semester['ramos']):
-            self.render_course(ramo, doc)
+            self.render_course(ramo)
 
-    def render_course(self, ramo, doc):
-        with doc.tag('td', klass='ramo'):
-            self.render_course_element('codigo', ramo.get('codigo'), doc)
-            self.render_course_element('nombre', ramo.get('nombre'), doc)
-            self.render_course_element('creditos', ramo.get('creditos'), doc)
+    def render_course(self, ramo):
+        with self.doc.tag('td', klass='ramo'):
+            self.render_course_element('codigo', ramo.get('codigo'))
+            self.render_course_element('nombre', ramo.get('nombre'))
+            self.render_course_element('creditos', ramo.get('creditos'))
 
-    @staticmethod
-    def render_course_element(name, data, doc):
-        with doc.tag('div', klass=name):
-            doc.text(data)
+    def render_course_element(self, name, data):
+        with self.doc.tag('div', klass=name):
+            self.doc.text(data)
 
 
 if __name__ == '__main__':
