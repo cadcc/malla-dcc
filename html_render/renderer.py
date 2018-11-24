18002e5a2b63f2c76f8eade52d63d97480cf4b97
Sergio Leiva <sergio.leiva.ocl@gmail.com>
2018-11-24 20:32:55 -0300

    Creación básica de la malla en html

--- html_render/renderer.py
+++ html_render/renderer.py
@@ -11,8 +11,8 @@ class Renderer:
 
     def render_program(self):
         doc = Doc()
-        with doc.tag('tr'):
-            for semester in iter(r.data.get('semestres')):
+        for semester in iter(r.data.get('semestres')):
+            with doc.tag('tr'):
                 self.render_semester(semester, doc)
         return doc.getvalue()
 
@@ -20,12 +20,20 @@ class Renderer:
         with doc.tag('td', klass='rotate'):
             doc.text('Semestre ' + semester.get('numero'))
         for ramo in iter(semester['ramos']):
-            self.render_ramo(ramo, doc)
+            self.render_course(ramo, doc)
 
-    def render_ramo(self, ramo, doc):
-        pass
+    def render_course(self, ramo, doc):
+        with doc.tag('td', klass='ramo'):
+            self.render_course_element('codigo', ramo.get('codigo'), doc)
+            self.render_course_element('nombre', ramo.get('nombre'), doc)
+            self.render_course_element('creditos', ramo.get('creditos'), doc)
+
+    @staticmethod
+    def render_course_element(name, data, doc):
+        with doc.tag('div', klass=name):
+            doc.text(data)
 
 
 if __name__ == '__main__':
     r = Renderer('../malla.json')
-    r.render_program()
+    print(r.render_program())
