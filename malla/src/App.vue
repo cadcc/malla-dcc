40b72068412ded05043febbce275f5c57920f58a
svandich <137367720+svandich@users.noreply.github.com>
2021-04-11 17:26:49 -0400

    added toggle to change contents

--- malla/src/App.vue
+++ malla/src/App.vue
@@ -3,10 +3,12 @@
     <div
       class="flex flex-col items-center justify-center dark:bg-gray-700 dark:text-white"
     >
-      <toggle class="ml-auto" :value="$store.state.darkMode" />
-      <p class="my-4 text-3xl font-bold text-center">
-        Malla DCC 💻 (vieja 🧓👴)
-      </p>
+      <toggle @toggle="$store.dispatch('toggleDarkMode')" id="darkMode" active-text="🌙" non-active-text="☀️" class="ml-auto" :value="$store.state.darkMode" />
+      <div class="my-4 text-3xl font-bold text-center flex">
+        <p>Malla DCC 💻 <span>{{$store.state.mallaActiva==="mallav3"?'(vieja 🧓👴)':'(nueva👧👦)'}}</span></p>
+      </div>
+      <p>Versión malla</p>
+      <toggle class="mb-4" @toggle="$store.dispatch('toggleMalla')" id="malla" active-text="v3" non-active-text="v5" :value="$store.state.mallaActiva==='mallav3'" />
       <div>
         <div class="flex flex-col items-center mb-4 md:flex-row">
           <div class="flex flex-row items-center mr-4">
@@ -21,7 +23,7 @@
             ></div>
             <span>Ramo requerido</span>
           </div>
-          <div class="flex flex-row items-center mr-4">
+          <div class="flex flex-row items-center mr-4" v-if="$store.state.mallaActiva==='mallav3'">
             <div
               class="w-8 h-4 mr-1 bg-yellow-300 border-2 border-black border-dotted dark:bg-yellow-600 dark:border-white"
             ></div>
@@ -61,10 +63,10 @@ export default {
     Semester,
     Toggle,
   },
-  data() {
-    return {
-      semesters: malla.semesters,
-    };
-  },
+  computed: {
+    semesters(){
+      return this.$store.state.mallaActiva==='mallav3'? mallav3.semesters: mallav5.semesters;
+    }
+  }
 };
 </script>
--- malla/src/components/toggle.vue
+++ malla/src/components/toggle.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center pt-4 pr-2">
-    <label for="toogleA" class="flex items-center cursor-pointer">
+  <div class="flex items-center pt-2 pr-2">
+    <label :for="id" class="flex items-center cursor-pointer">
       <div class="relative">
         <input
-          id="toogleA"
+          :id="id"
           type="checkbox"
           class="hidden"
-          @change="$store.dispatch('toggleDarkMode')"
+          @change="$emit('toggle')"
         />
         <div
           class="w-10 h-4 bg-gray-400 rounded-full shadow-inner toggle__line"
@@ -15,8 +15,8 @@
           class="absolute inset-y-0 left-0 w-6 h-6 bg-white rounded-full shadow toggle__dot"
         ></div>
       </div>
-      <div class="ml-3 font-medium text-gray-700">
-        {{ value ? '🌙' : '☀️' }}
+      <div class="ml-3 font-medium text-gray-700 dark:text-white">
+        {{ value ? activeText : nonActiveText }}
       </div>
     </label>
   </div>
@@ -38,6 +38,18 @@ input:checked ~ .toggle__dot {
 <script>
 export default {
   props: {
+    activeText: {
+      type: String,
+      required: true,
+    },
+    nonActiveText: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
     value: {
       type: Boolean,
       required: true,
